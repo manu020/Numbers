1d98a73d0578cd5dd44df13e4be7ae98efb15c1b
manohar <manohar961871@gmail.com>
2023-08-17 11:51:15 +0530

    logic created for EvenOrOdd

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,3 +1,16 @@
 public class EvenOrOdd {
 
+    public static boolean isEven(int num) {
+        if (num % 2 == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean isOdd(int num) {
+        if (num % 2 != 0)
+            return true;
+        else
+            return false;
+    }
 }
