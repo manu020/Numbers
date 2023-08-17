cda3fd62297cf8ce30e70e5e94d6d8b8aa823006
manohar <manohar961871@gmail.com>
2023-08-17 15:55:39 +0530

    created logic for Palindrome

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,14 @@
 public class Palindrome {
-    
+    public static void main(String[] args) {
+        int num=123215;
+        int sum = 0, temp = 0;
+        int n=num;
+        while (num > 0) {
+            temp = num % 10;
+            sum = (sum * 10) + temp;
+            num = num / 10;
+        }
+        //System.out.println(sum);
+         System.out.println((n==sum) ? "palindrome":"not palindrome");
+    }
 }
