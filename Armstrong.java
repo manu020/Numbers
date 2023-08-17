fa5f9cd363bf33097ac4a139b4447c18c0e924c8
manohar <manohar961871@gmail.com>
2023-08-17 15:45:44 +0530

    created logic for armstrong number

--- Armstrong.java
+++ Armstrong.java
@@ -1,3 +1,19 @@
 public class Armstrong{
-    
+    public static void main(String[] args) {
+        int num = 1634;    
+    int sum=0,temp=0;
+    int n=num; int number=num;
+    while(num>0){
+       num=num/10;
+       sum=sum+1;
+    }
+   
+     while(number>0){
+       int last =number%10;
+        temp=(int) (temp+(Math.pow(last,sum)));
+        number=number/10;
+     }
+     System.out.println((n==temp)?"Armstrong number":"Not Armstrong number");
+     
+    }
 }
